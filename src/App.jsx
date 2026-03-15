6e7d18a3d1ca279831316e8bdd09e9d6185c34e4
feifei12359 <160389708+feifei12359@users.noreply.github.com>
2026-03-15 11:32:40 +0800

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './Home.jsx';
 import TrendDetailPage from './pages/TrendDetailPage.jsx';
 import TrendsPage from './pages/TrendsPage.jsx';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/trends" element={<TrendsPage />} />
         <Route path="/trends/:keyword" element={<TrendDetailPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
